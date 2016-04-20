24acad19598669c366f58e7694d3a9d615270102
linxingchen <linxingchen@baidu.com>
2016-04-20 10:45:25 +0800

    init

---
+++ application/models/AlumniModels.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bricks
+ * Date: 15/12/30
+ * Time: 下午11:11
+ */
+
+
+
+class AlumniModels extends CI_Model{
+    private static $tableName = "alumni";
+    public function __construct(){
+        parent::__construct();
+        $this->load->database();
+        $this->load->model('UserLogModels');
+    }
+
+    /*
+     ** 新增同学录
+     *
+     * @param $userId
+     * @param $title
+     * @param $cover 封面模板
+     * @return bool
+     */
+    public function addAlumni($userId, $title = '', $cover = 0){
+        $this->db->trans_start();
+        $this->db->insert(
+            self::$tableName, array(
+            'user_id'     => $userId,
+            'title'       => $title,
+            'cover'       => $cover,
+            'create_time' => time(),
+            'update_time' => time(),
+        ));
+
+        $id = $this->db->insert_id();
+
+        //打log
+        $logContent = array(
+            'alumni_id' => $id,
+            'user_id'   => $userId,
+            'title'     => $title,
+            'des'       => "add alumni",
+        );
+
+
+        $this->db->trans_complete();
+        if (!$this->db->trans_status()){
+            $logContent['run_status'] = 0;
+        } else {
+            $logContent['run_status'] = 1;
+        }
+        $this->UserLogModels->addUserLog($userId, $logContent, self::$tableName, __METHOD__);
+        return $logContent['run_status'];
+
+    }
+
+    /*
+     *
+     * 修改同学录
+     *
+     *@param $alumniId
+     *@param $userId
+     *@param $alumniParams
+     */
+    public function updateAlumni($alumniId, $userId, $title = false, $cover = false){
+        $alumniUpdate = array();
+        if(false != $title){
+            $alumniUpdate['title']  = $title;
+        }
+        if(false != $cover){
+            $alumniUpdate['cover'] = $cover;
+        }
+
+        $alumniUpdate['update_time'] = time();
+
+        $this->db->trans_start();
+
+        $this->db->where('id', $alumniId);
+        $this->db->update(self::$tableName, $alumniUpdate);
+
+        //打log
+        $alumniUpdate['affected_rows'] = $this->db->affected_rows();
+        $alumniUpdate['des'] = "update Alumni table";
+
+        $this->db->trans_complete();
+        if (!$this->db->trans_status()){
+            $alumniUpdate['run_status'] = 0;
+        } else {
+            $alumniUpdate['run_status'] = 1;
+        }
+        $this->UserLogModels->addUserLog($userId, $alumniUpdate, self::$tableName, __METHOD__);
+        return $alumniUpdate['run_status'];
+    }
+
+}
---
+++ application/models/UserModels.php
@@ -0,0 +1,188 @@
+<?php
+/**
+ * 用户表
+ *
+ * Created by PhpStorm.
+ * User: lin
+ * Date: 15-11-10
+ * Time: 下午11:26
+ */
+class UserModels extends CI_Model{
+    private static $tableName = 'user';
+
+    public function __construct(){
+        parent::__construct();
+        $this->load->database();
+        $this->load->model('UserLogModels');
+        $this->load->library('util/Uuid');
+    }
+
+
+    /**
+     * 新增用户
+     *
+     * @param $userPW
+     * @param $userMobile
+     * @param $userEmail
+     * @return bool
+     */
+    public function addUser($userPW, $userMobile, $userEmail){
+        $this->db->trans_start();
+        $userId = Uuid::genUUID(CoreConst::USER_UUID);
+        $this->db->insert(self::$tableName, array(
+            'user_id'           => $userId,
+            'user_password'     => $userPW,
+            'user_create_time'  => time(),
+            'user_status'       => 1,
+            'user_mobile'       => $userMobile,
+            'user_email'        => $userEmail,
+        ));
+
+        //打log
+        $logContent = array(
+            'user_mobile'   => $userMobile,
+            'user_email'    => $userEmail,
+        );
+
+
+        $this->db->trans_complete();
+        if (!$this->db->trans_status()){
+            $logContent['run_status'] = 0;
+        } else {
+            $logContent['run_status'] = 1;
+        }
+        $this->UserLogModels->addUserLog($userId, $logContent, self::$tableName, __METHOD__);
+        return $logContent['run_status'];
+    }
+
+    /**
+     * 检查用户是否已经存在
+     *
+     *
+     * @param $userMobile
+     * @param $userEmail
+     */
+    public function checkUserExists($userMobile, $userEmail){
+        $this->db->where('user_mobile', $userMobile);
+        $this->db->or_where('user_email', $userEmail);
+        return $this->db->count_all_results(self::$tableName);
+    }
+
+
+    /**
+     * 获取用户基本信息
+     *
+     * @param $userId
+     * @return array
+     */
+    public function getUserBasicInfo($userId){
+        $this->db->where('user_id', $userId);
+        $query  = $this->db->get(self::$tableName);
+        $result = $query->row_array();
+        return $result;
+    }
+
+    /**
+     * 获取用户基本信息
+     *
+     * @param $userIdList
+     * @return array
+     */
+    public function getUserBasicInfoList($userIdList){
+        if(empty($userIdList))
+        {
+            return array();
+        }
+        $this->db->select('user_id, user_name');
+        $this->db->where_in('user_id', $userIdList);
+        $query  = $this->db->get(self::$tableName);
+        $result = $query->result_array();
+        return $result;
+    }
+
+
+
+    /**
+     * 通过登陆的手机号或邮箱获取用户信息
+     *
+     * @param $userLoginName
+     * @return Array
+     */
+    public function getUserInfoByLoginName($userLoginName)
+    {
+        $this->db->where('user_mobile', $userLoginName);
+        $this->db->or_where('user_email', $userLoginName);
+        $query  = $this->db->get(self::$tableName);
+        $result = $query->row_array();
+        return $result;
+    }
+
+    /**
+     * 修改用户基础数据
+     *
+     * @param $userId
+     * @param $userName
+     * @param $userBirthday
+     * @param $userSex
+     * @param $userMobile
+     * @param $userEmail
+     * @param $userSign
+     * @param $userStatus
+     * @return bool
+     */
+    public function updateUser($userId, $userName = false, $userBirthday = false, $userSex = false,
+                               $userMobile = false, $userEmail = false, $userSign = false, $userStatus = false){
+
+        $arrUpdateConds = array();
+
+        if (!empty($userName)){
+            $arrUpdateConds['user_name']     = $userName;
+        }
+
+        if (!empty($userBirthday)){
+            $arrUpdateConds['user_birthday'] = $userBirthday;
+        }
+
+        if (FALSE !== ($userSex)){
+            $arrUpdateConds['user_sex']      = $userSex;
+        }
+
+        if (!empty($userMobile)){
+            $arrUpdateConds['user_mobile']   = $userMobile;
+        }
+
+        if (!empty($userEmail)){
+            $arrUpdateConds['user_email']    = $userEmail;
+        }
+
+        if (!empty($userSign)){
+            $arrUpdateConds['user_sign']     = $userSign;
+        }
+
+        if (FALSE !== $userStatus){
+            $arrUpdateConds['user_status']   = $userStatus;
+        }
+
+        if (empty($arrUpdateConds)){
+            return 0;
+        }
+
+        $this->db->trans_start();
+
+        $this->db->where('user_id', $userId);
+        $this->db->update(self::$tableName, $arrUpdateConds);
+
+        //打log
+        $arrUpdateConds['affected_rows'] = $this->db->affected_rows();
+
+        $this->db->trans_complete();
+        if (!$this->db->trans_status()){
+            $arrUpdateConds['run_status'] = 0;
+        } else {
+            $arrUpdateConds['run_status'] = 1;
+        }
+        $this->UserLogModels->addUserLog($userId, $arrUpdateConds, self::$tableName, __METHOD__);
+        return $arrUpdateConds['run_status'];
+    }
+
+}
